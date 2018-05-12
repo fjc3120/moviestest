c838dfab10a25d8a378eaf82e2be1fdbb5cfe2d8
fjc3120 <287473791@qq.com>
2018-05-12 22:53:35 +0800

    k

--- src/components/photo/Photo.vue
+++ src/components/photo/Photo.vue
@@ -1,9 +1,14 @@
 <template>
-<h1>photo</h1>
+<ul>
+  <li v-for="item in photos" :key="item.id">
+    <img :src="item.src "alt="">
+  </li>
+</ul>
 </template>
 
 <script>
   import store from  '@/vuex/store.js'
+  import axios from 'axios'
   export default({
     store,
     created(){
@@ -11,10 +16,36 @@
         color:"black",
         title:"图片",
       });
+      this.getData();
+    },
+    data(){
+      return{
+          photos:[],
+      }
+    },
+
+    methods:{
+        getData(){
+            axios.get('./static/data/photodata.json')
+              .then((res)=>{
+               this.photos = res.data.photoData
+              }).catch((res)=>{
+                console.log("错误");
+            })
+        }
     }
   })
 </script>
 
 <style scoped>
+ul{
 
+}
+  ul li{
+float: left;
+    width: 50%;
+  }
+ul li img{
+
+}
 </style>
